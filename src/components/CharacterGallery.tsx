823451c01005f76d82b0cd45203d37cac70c537d
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-03-07 11:15:23 +0100

    inital commit

---
+++ src/components/CharacterGallery.tsx
@@ -0,0 +1,17 @@
+import {Character} from "../types/RickAndMortyCharacter.ts";
+import CharacterCard from "./CharacterCard.tsx";
+import "./CharacterGallery.css";
+
+type CharacterGalleryProps =
+{
+    characters: Character[];
+}
+export default function CharacterGallery(props: Readonly<CharacterGalleryProps>)
+{
+    const cards = props.characters.map((character) => <CharacterCard key={character.name} character={character}/>);
+    return (
+        <div className="character-gallery">
+            {cards}
+        </div>
+    );
+}
